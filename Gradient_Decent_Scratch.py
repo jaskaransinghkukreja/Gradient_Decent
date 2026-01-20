f979a043b372ae59ca403aa98d8a3be1b6e27615
jaskaransinghkukreja <jaskaransinghkukreja@gmail.com>
2026-01-20 19:11:59 +0530

    Add files via upload

--- Gradient_Decent_Scratch.py
+++ Gradient_Decent_Scratch.py
@@ -1,4 +1,4 @@
-class Gradient_Decent_Scratch:  
+class Gradient_Decent_Scratch:       # Gradient Decent Code from Scratch using class     
     def __init__(self,m,b,lr,epochs):
         self.m=m
         self.b=b
@@ -28,18 +28,18 @@ class Gradient_Decent_Scratch:
             pred_2=self.m * X +self.b
             plt.plot(X,pred_2,color="green")
         
-        print(self.b)
-        print(self.m)
+        print(f'Intercept of the Best Fit Line using our class:{self.b}')
+        print(f'Slope of the Best Fit Line using our class:{self.m}')
         
         pred_3=self.m * X +self.b
-        plt.plot(X,pred_3,color="purple",label="Final BFL")
-        plt.legend()
-        plt.show()
+        plt.plot(X,pred_3,color="purple",label="Class BFT")
+        
 
 
 
 
 
+#Training model using Scikit learn and calling Class(Gradient_Decent_Scratch)
 
 import pandas  as pd
 import matplotlib.pyplot as plt
@@ -51,7 +51,17 @@ lr= LinearRegression()
 X,y=make_regression(n_samples=100,n_features=1,n_informative=1,n_targets=1,noise=52,random_state=42)
 
 lr.fit(X,y)
-print(f'Intercept : {lr.intercept_}\nCoefficient : {lr.coef_}')
 
-model=Gradient_Decent_Scratch(-100,-10,0.001,10)
+b=float(input("Enter the value of the Intercept:"))
+m=float(input("Enter the value of the Slope:"))
+learn_rate=float(input("Enter the Learning Rate:"))
+epochs=int(input("Enter the Epochs(number of iterations):"))
+
+model=Gradient_Decent_Scratch(m,b,learn_rate,epochs)
 model.fit(X,y)
+
+y_pred=lr.predict(X)
+plt.plot(X,y_pred,color="magenta",label="Original BFL")
+plt.legend()
+plt.show()
+print(f'Intercept(Scikit-learn model): {lr.intercept_}\nCoefficient(Scikit-learn model): {lr.coef_}')
